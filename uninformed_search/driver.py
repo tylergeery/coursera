56af8da0280659dc2096cc532661ce8b66057d4e
tylergeery <tyler.geery@yahoo.com>
2017-01-27 23:04:45 -0800

    finish bfs

--- uninformed_search/driver.py
+++ uninformed_search/driver.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 import sys
+import resource
+import time
 
 # convenience
 def i_arr_to_s(i_arr): return ','.join(map(str, i_arr))
@@ -58,11 +60,13 @@ class Board:
 
     def move_allowed(self, move):
         zero_index = self.board.index(0)
+        #print "Adding move ({0}) found 0-index ({1}):".format(move, zero_index)
+
 
         if move == 'L':
-            return (zero_index % 3 != 0)
+            return (zero_index % 3) != 0
         if move == 'R':
-            return (zero_index % 3 != 2)
+            return (zero_index % 3) != 2
         if move == 'U':
             return (zero_index > 2)
         if move == 'D':
@@ -82,10 +86,11 @@ class Board:
         if move == 'U':
             swap_index = zero_index - 3
 
-        self.board[zero_index] = self.board[swap_index]
-        self.board[swap_index] = 0
+        board = list(self.board) # mutable
+        board[zero_index] = board[swap_index]
+        board[swap_index] = 0
 
-        return i_arr_to_s(self.board)
+        return i_arr_to_s(board)
 
 
 
@@ -95,8 +100,12 @@ class Board:
 Solver class will solve and output
 """
 class Solver:
+    time_started = time.clock()
+    frontier = []
     solution = False
     nodes_expanded = 0
+    max_fringe_size = 0
+    max_search_depth = 0
 
     def __init__(self, method, board):
         if method == 'bfs':
@@ -107,16 +116,18 @@ class Solver:
         self.output()
 
     def solve_bfs(self, board):
-        frontier = [(board, '')] #tuples containing (board, path)
-        states_visited = [] #dont requeue states (states are boards stringified)
+        self.frontier.append((board, '')) # add initial state
+        states_visited = [board] #dont requeue states (states are boards stringified)
 
-        while len(frontier):
+        while len(self.frontier):
             #pop from List and create state
-            state_tuple = frontier.pop(0)
+            self.max_fringe_size = max(self.max_fringe_size, len(self.frontier))
+            state_tuple = self.frontier.pop(0)
             state = State(state_tuple)
             self.nodes_expanded += 1
-            print "State Board:", state.board.board
-            print "State Path:", state.path
+            self.max_search_depth = max(self.max_search_depth, len(state_tuple[1]))
+            #print "State Board:", state.board.board
+            #print "State Path:", state.path
 
             #decide if new board solves problem
             if state.board.is_complete():
@@ -125,13 +136,16 @@ class Solver:
 
             #queue neighbor states, if applicable
             for move in ['U', 'D', 'L', 'R']:
-                if state.board.move_allowed(move) and state_tuple[0] not in states_visited:
-                    print "Adding state:", state_tuple[0]
+                if state.board.move_allowed(move):
                     next_state = (state.board.get_board_after_move(move), state.path + move)
-                    frontier.append(next_state)
-                    states_visited.append(state_tuple[0])
-                else:
-                    print "Cant add move ({0}) for state ({1}):".format(move, state_tuple[0])
+                    if next_state[0] not in states_visited:
+                        #print "Adding state to frontier:", next_state[0], move
+                        self.frontier.append(next_state)
+                        states_visited.append(next_state[0])
+                    #else:
+                        #print "Cant add move ({0}) for state ({1}):".format(move, next_state[0])
+                #else:
+                    #print "Move not allowed ({0}) for state ({1}):".format(move, state_tuple[0])
 
     def solve_dfs(self):
         return
@@ -143,6 +157,12 @@ class Solver:
         print 'path_to_goal:', self.solution.get_path()
         print 'cost_of_path:', len(self.solution.path)
         print 'nodes_expanded:', self.nodes_expanded
+        print 'fringe_size:', len(self.frontier)
+        print 'max_fringe_size:', self.max_fringe_size
+        print 'search_depth:', len(self.solution.path)
+        print 'max_search_depth:', self.max_search_depth
+        print 'running_time:', time.clock() - self.time_started
+        print 'max_ram_usage:', resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
 
 
 if __name__ == "__main__":
